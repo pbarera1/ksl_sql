f7565582f4524d7d8fe684ae1097993bdd08e31b
Barera <Phil.Barera@kiscosl.com>
2025-10-14 13:06:20 -0700

    dim user

--- PopulateDW/original-dim_user.sql
+++ PopulateDW/original-dim_user.sql
@@ -2,33 +2,25 @@
 --###################################################### Fill in Dim_User  #####################################################################
 --########################################################################################################################################################
 --TRUNCATE TABLE Dim_User;
-;WITH CTE (
-	ksl_CommunityIdName
-	,ksl_CommunityId
-	,FullName
-	,Title
-	,InternalEmailAddress
-	,DomainName
-	,isUserActive
-	,SystemUserId
-	,dupcnt
-	)
-AS (
-	SELECT ksl_CommunityIdName
-		,ksl_CommunityId
-		,FullName
-		,Title
-		,InternalEmailAddress
-		,DomainName
-		,IIF(isDisabled = 1, 'No', 'Yes') AS isUserActive
-		,SystemUserId
-		,ROW_NUMBER() OVER (
-			PARTITION BY internalemailaddress ORDER BY isDisabled ASC
-				,len(FullName) ASC
-			) AS dupcnt
-	FROM kslcloud_mscrm.dbo.systemuser
-	) --select * from CTE where dupcnt = 1
---INSERT INTO Dim_User
+;WITH CTE(ksl_CommunityIdName, ksl_CommunityId, FullName, Title, InternalEmailAddress, DomainName, isUserActive, SystemUserId, dupcnt) AS (
+		SELECT c.name
+			,c.CRM_CommunityID
+			,a.USR_First + ' ' + a.USR_Last
+			,r.Name
+			,a.USR_Email
+			,a.USR_Email
+			,IIF(a.USR_Active = 1, 'Yes', 'No') AS isUserActive
+			,a.SalesAppId
+			,ROW_NUMBER() OVER (
+				PARTITION BY a.USR_Email ORDER BY a.USR_Active DESC
+					,LEN(a.USR_First + ' ' + a.USR_Last) ASC
+				) AS dupcnt
+		FROM KiscoCustom.dbo.Associate a
+		JOIN KiscoCustom.dbo.KSL_Roles r ON r.RoleID = a.RoleID
+		JOIN KiscoCustom.dbo.Community c ON c.CommunityIDY = a.USR_CommunityIDY
+		WHERE a.SalesAppID IS NOT NULL -- bring count down from 2890 -> 819
+		)
+
 SELECT ksl_CommunityIdName
 	,ksl_CommunityId
 	,FullName
