e22637952beb4113325387d4f429a1b66e32853f
Barera <Phil.Barera@kiscosl.com>
2025-10-16 09:20:10 -0700

    check sip

--- SIP/updated-HereWholeQtr.sql
+++ SIP/updated-HereWholeQtr.sql
@@ -2,7 +2,7 @@
 --declare @AsOfDate Date = '01-01-2025';
 
 SELECT c.ksl_name
-	,u.fullname
+	,(u.USR_First + ' ' + u.USR_Last) AS fullname
 	,count(*)
 FROM ksldb252.datawarehouse.dbo.budgets b
 INNER JOIN KiscoCustom.dbo.Associate u ON b.description = u.USR_Email
@@ -11,4 +11,4 @@ WHERE b.dt >= DATEADD(qq, DATEDIFF(qq, 0, @AsOfDate), 0)
 	AND b.dt <= DATEADD(dd, - 1, DATEADD(qq, DATEDIFF(qq, 0, @AsOfDate) + 1, 0))
 	AND budget = '0'
 GROUP BY c.ksl_name
-	,u.fullname
+	,(u.USR_First + ' ' + u.USR_Last)
